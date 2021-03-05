262f7a7df9324c9d2080f258e289af7df1dbd912
koichi.tachizaki <koichi.tachizaki@person-link.co.jp>
2021-03-05 20:46:06 +0900

    init push

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <?php wp_head(); ?>
+</head>
+<body>
+  <header>
+  </header>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+<main>
+  <?php have_posts(); ?>
+  <?php get_posts(); ?>
+</main>
+<?php get_footer(); ?>
